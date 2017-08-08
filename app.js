e7d3a31eb52a122761a6c999a514ef2ecd2aa7ab
thekevinliao <kev.revolution@gmail.com>
2017-08-08 09:25:28 -0400

    Personal website

---
+++ app.js
@@ -0,0 +1,40 @@
+
+function showIntro(pauseTime){
+        // alert('hi');
+		    $('#int').fadeIn(500);
+		setTimeout(function(){ 
+			$('#int0').fadeIn(700)}, pauseTime*5);
+		setTimeout(function(){ 
+			$('#int1').fadeIn(700)}, pauseTime*6);
+		setTimeout(function(){ 
+			$('#int2').fadeIn(700)}, pauseTime*7);
+		setTimeout(function(){ 
+			$('#int3').fadeIn(700)}, pauseTime*8);
+        setTimeout(function(){ 
+			$('#int4').fadeIn(700)}, pauseTime*9);
+        setTimeout(function(){ 
+			$('#int5').fadeIn(700)}, pauseTime*14);
+        setTimeout(function(){ 
+			$('#int6').fadeIn(700)}, pauseTime*15);
+        setTimeout(function(){ 
+			$('#int7').fadeIn(700)}, pauseTime*16);
+        setTimeout(function(){ 
+			$('#int8').fadeIn(700)}, pauseTime*17);
+}
+
+showIntro(200);
+
+//  <div id="des">
+//         <p>My name is Kevin Liao, and I am an avid programmer and have a passion for technology. I have been coding for years, but I recently started working on projects at the beginning of high school when I joined Stuyvesant High School'school
+//             FRC robotics team, Stuypulse team 694. My interest in technology stemmed from my first touch of a computer when I was 5, and continued using it ever since. The person who introduced me to code was my brother. He 
+//             explained to me what technology was really about, and what was happening behind the screen of the devices we use everyday. Since that day, I have been devoted to figuring out how things work.</p>
+//     </div>
+
+
+
+
+
+
+
+
+
